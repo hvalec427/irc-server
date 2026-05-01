a2f3726339ea03e0d1f887dd6f5f37be847080bb
Žiga Hvalec <zigahvalec@gmail.com>
2026-05-01 19:51:00 +0200

    add NICK command

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import net from "net";
 
 const server = net.createServer((socket) => {
+    let nick = "";
+
     console.log("client connected");
 
     socket.write(":irc-server NOTICE * :Serbus from my IRC server\r\n");
@@ -14,13 +16,24 @@ const server = net.createServer((socket) => {
             console.log("received:", line);
 
             if (line.startsWith("PING")) {
-                socket.write("PONG :irc-server\r\n");
+                if (nick) {
+                    socket.write(`PONG ${nick}\r\n`);
+                } else {
+                    socket.write("PONG :irc-server\r\n");
+                }
+            }
+
+            if (line.startsWith("NICK ")) {
+                nick = line.split(" ")[1];
+                console.log("nickname set to:", nick);
+
+                socket.write(`:irc-server NOTICE ${nick} :Nickname set to ${nick}\r\n`);
             }
         }
     });
 
     socket.on("close", () => {
-        console.log("client disconnected");
+        console.log(`${nick || "client"} disconnected`);
     });
 });
 
